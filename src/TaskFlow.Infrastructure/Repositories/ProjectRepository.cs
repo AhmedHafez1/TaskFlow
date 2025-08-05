16b0f78ab54cc7cde00c54455cc201a7f165d1a4
EMEA\IBA1SF <fixed-term.ahmad.ibraheem@bosch.com>
2025-08-05 18:29:53 +0300

    Project Repo

--- src/TaskFlow.Infrastructure/Repositories/ProjectRepository.cs
+++ src/TaskFlow.Infrastructure/Repositories/ProjectRepository.cs
@@ -1,12 +1,32 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TaskFlow.Application.Interfaces.Repositories;
+using TaskFlow.Domain.Entities;
+using TaskFlow.Infrastructure.Data;
 
 namespace TaskFlow.Infrastructure.Repositories
 {
-    public class ProjectRepository
+    public class ProjectRepository : GenericRepository<Project>, IProjectRepository
     {
-        
+        private readonly TaskFlowDbContext _context;
+
+        public ProjectRepository(TaskFlowDbContext context)
+            : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<Project?> GetByNameAsync(string name)
+        {
+            var project = await _context.Projects.Where(p => p.Name == name).FirstOrDefaultAsync();
+            return project;
+        }
+
+        public async Task<Project?> GetByOwnerIdAsync(int ownerId)
+        {
+            var project = await _context
+                .Projects.Where(p => p.OwnerId == ownerId)
+                .FirstOrDefaultAsync();
+            return project;
+        }
     }
 }
